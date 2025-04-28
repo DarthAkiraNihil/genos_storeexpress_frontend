d4ca6930f9d56b289dcc53d7d51a412f1a9f22f3
DarthAkiraNihil <egorzverev666@gmail.com>
2025-04-28 21:28:18 +0300

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+﻿import React, { FC, ReactElement } from "react";
+import { Box, Container, Grid, Typography } from "@mui/material";
+
+export const Footer: FC = (): ReactElement => {
+    return (
+        <Box
+            sx={{
+                width: "100%",
+                height: "auto",
+                backgroundColor: "secondary.main",
+                paddingTop: "1rem",
+                paddingBottom: "1rem",
+                bottom: 0,
+            }}
+        >
+            <Container maxWidth="lg">
+                <Grid container direction="column" alignItems="center">
+                    <Grid size={12}>
+                        <Typography color="black" variant="h5">
+                            GenosStorExpress
+                        </Typography>
+                    </Grid>
+                    <Grid size={12}>
+                        <Typography color="textSecondary" variant="subtitle1">
+                            {`${new Date().getFullYear()} | GS CO., powered by D.A.N. Corporation Alliance | Akira Nihil | All Rights Reserved`}
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Box>
+    );
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export { default } from './GenosStorExpressLayout'
+export { Footer } from './Footer';
